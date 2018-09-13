5268ac77755928a0b99114a8d96675a767792546
Rachel <rldet527@gmail.com>
2018-09-13 03:15:15 -0500

    Syntax - Parenthesis
    Was missing print parenthesis. Added them.

--- weaponClassCrossbow.py
+++ weaponClassCrossbow.py
@@ -8,42 +8,42 @@ class weapon (object):
 class sword(weapon):
     """Derived class of weapon"""
     def swing(self):
-        print "Swing does", self.damage ,"damage"
+        print ("Swing does", self.damage ,"damage")
 
 class crossbow(weapon):
     def __init__(self,arrow):
         self.arrow = arrow
 ##        if arrow == 1:
-##            print "Crossbow is loaded.\n"
+##            print ("Crossbow is loaded.\n")
 ##        elif arrow == 0:
-##            print "This is an unloaded crossbow.\n"
+##            print ("This is an unloaded crossbow.\n")
 ##        elif arrow > 1:
-##            print "You can only load one arrow at a time. Let me help you."
+##            print ("You can only load one arrow at a time. Let me help you.")
 ##            arrow = 1
-##            print "Alright, one arrow is loaded.\n"
+##            print ("Alright, one arrow is loaded.\n")
 ##        else:
-##            print "Invalid response. You'll have to load an arrow later.\n"
+##            print ("Invalid response. You'll have to load an arrow later.\n")
 ##            arrow = 0
     def is_loaded(self):
         if self.arrow == 0:
-            print "Crossbow isn't loaded. Load an arrow.\n"
+            print ("Crossbow isn't loaded. Load an arrow.\n")
         elif self.arrow == 1:
-            print "Crossbow is locked and loaded. Fire away!\n"
+            print ("Crossbow is locked and loaded. Fire away!\n")
 
 
     def fire(self):
         if self.arrow == 1:
             self.arrow -= 1
-            print "You shot an arrow!\n"
+            print ("You shot an arrow!\n")
         elif self.arrow == 0:
-            print "There's nothing in the chamber to shoot.\n"
+            print ("There's nothing in the chamber to shoot.\n")
 
     def reload(self):
         if self.arrow < 1:
             self.arrow += 1
-            print "Loading a new arrow.\n"
+            print ("Loading a new arrow.\n")
         else:
-            print "You already have an arrow in the chamber. Fire it!\n"
+            print ("You already have an arrow in the chamber. Fire it!\n")
 
 def main():
     crossbow1 = crossbow(0)
